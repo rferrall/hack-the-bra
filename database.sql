443b5919be6fd70a4478f6f2f8c4aae5103d9eb4
i-vespa <esawv.student@gmail.com>
2019-01-26 15:22:52 -0600

    adding database

--- database.sql
+++ database.sql
@@ -5,3 +5,41 @@ CREATE TABLE "person" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE user_account (
+    id SERIAL PRIMARY KEY,
+    username text NOT NULL UNIQUE,
+    password text NOT NULL
+);
+
+-- Indices -------------------------------------------------------
+
+CREATE UNIQUE INDEX user_account_pkey ON user_account(id int4_ops);
+CREATE UNIQUE INDEX user_account_username_key ON user_account(username text_ops);
+
+
+
+
+CREATE TABLE user_bras (
+    id SERIAL PRIMARY KEY,
+    user_id integer REFERENCES user_account(id),
+    image_url text
+);
+
+-- Indices -------------------------------------------------------
+
+CREATE UNIQUE INDEX user_bras_pkey ON user_bras(id int4_ops);
+
+CREATE TABLE user_measurements (
+    id SERIAL PRIMARY KEY,
+    userid integer REFERENCES user_account(id),
+    name text,
+    band_size numeric,
+    bust_size numeric,
+    description text,
+    time timestamp without time zone NOT NULL
+);
+
+-- Indices -------------------------------------------------------
+
+CREATE UNIQUE INDEX user_measurements_pkey ON user_measurements(id int4_ops);
