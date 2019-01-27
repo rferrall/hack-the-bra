dc9e063a08e69c9c4fc2ba9fb6783f877483b6c6
i-vespa <esawv.student@gmail.com>
2019-01-26 20:04:40 -0600

    Van: adding drawer ui stuff

--- src/components/Views/Drawer/Drawer.js
+++ src/components/Views/Drawer/Drawer.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
+import Bras from '../../../jsonData/bras.json';
+import "./Drawer.css";
 // this could also be written with destructuring parameters as:
 // const UserPage = ({ user }) => (
 // and then instead of `props.user.username` you could use `user.username`
 const Drawer = (props) => (
+
+
+
   <div>
-    <h1 id="welcome">
-      Welcome, { props.user.username }!
-    </h1>
-    <p>Your ID is: {props.user.id}</p>
+  <h2 class="drawer-title"> My Drawer</h2>
+     <div class="flex-container">
+
+     {Bras.map((x, index) =>
+            <div class="card" key={index}>
+              <img src={x.image} alt="Bra" width="100%" />
+              <div class="container">
+
+                <h4><b>{x.title}</b></h4>
+                <p>{x.size}</p>
+                <p>{x.brand}</p>
+              </div>
+            </div>)}
+
+
+  </div>
   </div>
 );
 
@@ -22,3 +38,4 @@ const mapStateToProps = state => ({
 
 // this allows us to use <App /> in index.js
 export default connect(mapStateToProps)(Drawer);
+
